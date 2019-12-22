d66fb33d4e481f41a25b3c264f86ec6929d31ee2
gabriel <aballaygabriel@gmail.com>
2019-12-22 11:21:09 -0300

    Actualizacion base de datos

--- mascotas.sql
+++ mascotas.sql
@@ -1,21 +1,11 @@
 -- phpMyAdmin SQL Dump
-<<<<<<< HEAD
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-11-2019 a las 15:58:41
+-- Tiempo de generación: 22-12-2019 a las 15:20:11
 -- Versión del servidor: 10.1.35-MariaDB
 -- Versión de PHP: 7.2.9
-=======
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 19-11-2019 a las 03:59:28
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.8
->>>>>>> 347700ac7e0f3e361b2eed4be3e4c6994a987b4a
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,30 +25,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `encontrada`
+-- Estructura de tabla para la tabla `encontradas`
 --
 
-CREATE TABLE `encontrada` (
+CREATE TABLE `encontradas` (
   `EncontradaId` int(11) NOT NULL,
   `Foto` varchar(40) NOT NULL,
   `Descripcion` text NOT NULL,
-  `Fecha` datetime NOT NULL,
-  `LocalizacionId` int(11) NOT NULL,
-  `MascotaId` int(11) NOT NULL,
+  `Fecha` varchar(30) NOT NULL,
+  `Lugar` varchar(40) NOT NULL,
   `UsuarioId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `localizacion`
+-- Volcado de datos para la tabla `encontradas`
 --
 
-CREATE TABLE `localizacion` (
-  `LocalizacionId` int(11) NOT NULL,
-  `Latitud` decimal(10,0) NOT NULL,
-  `Longitud` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `encontradas` (`EncontradaId`, `Foto`, `Descripcion`, `Fecha`, `Lugar`, `UsuarioId`) VALUES
+(1, 'pet_frGW6vvi.jpg', 'otra cosa ', '6/12/2019', 'aca', 13),
+(2, 'pet_QlFvgENZ.jpg', 'otra cosa ', '6/12/2019', 'otro', 13),
+(3, 'pet_k9lKNwxE.jpg', 'a ver que sale ', '6/12/2019', 'segunda prueba', 13),
+(4, 'pet_PXl7zvGi.jpg', 'hjkkutf', '4/12/2019', 'hola', 13),
+(5, 'pet_x4bPH6Lb.jpg', 'esta alla', '6/12/2019', 'las chacras', 13),
+(6, 'pet_2vFeNtPF.jpg', 'sfghj', '6/12/2019', 'gghj', 13),
+(7, 'pet_sGrU9Lts.jpg', ' fghjkk', '6/12/2019', 'fggh', 13),
+(8, 'pet_bsSgbpkU.jpg', 'tuuu', '6/12/2019', 'ghk', 13),
+(9, 'pet_hldYd5a5.jpg', 'ghklll', '6/12/2019', 'hola', 13),
+(10, 'pet_AtZpO6SH.jpg', 'gjll', '6/12/2019', 'asi', 13),
+(11, 'pet_XeRQLgvA.jpg', 'hhjjj\ngghhjj\nfghh', '4/12/2019', 'aaaa', 13),
+(12, 'pet_jfgtdzCB.jpg', 'rtuikdfh\nftuii', '7/12/2019', 'fhkll', 13);
 
 -- --------------------------------------------------------
 
@@ -70,33 +65,46 @@ CREATE TABLE `mascotas` (
   `MascotaId` int(11) NOT NULL,
   `NombreMascota` varchar(40) NOT NULL,
   `Raza` varchar(30) NOT NULL,
-  `Tamaño` varchar(30) NOT NULL,
+  `Tamanio` varchar(30) NOT NULL,
   `Edad` int(11) NOT NULL,
   `Descripcion` text NOT NULL,
   `Foto` varchar(40) NOT NULL,
-  `Estado` varchar(30) NOT NULL,
-  `Fecha` datetime NOT NULL,
+  `Estado` int(11) NOT NULL,
+  `Fecha` varchar(30) NOT NULL,
   `RecompensaId` int(11) DEFAULT NULL,
-  `PerdidoEn` int(11) NOT NULL,
+  `Lugar` varchar(40) NOT NULL,
   `UsuarioId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `mascotas`
+--
+
+INSERT INTO `mascotas` (`MascotaId`, `NombreMascota`, `Raza`, `Tamanio`, `Edad`, `Descripcion`, `Foto`, `Estado`, `Fecha`, `RecompensaId`, `Lugar`, `UsuarioId`) VALUES
+(1, 'mono', 'dogo', 'mediano ', 2, 'blanco sin señas', 'pet_X7KTWMWb.jpg', 1, '5/12/2019', 1, 'las chacras ', 13),
+(2, 'tigre', 'gato siames', 'chico', 3, 'ea color negro y muy amigable', 'pet_tfVsqyph.jpg', 1, '3/12/2019', 1, 'San Luis centro', 13),
+(3, 'chiquito', 'galgo', 'mediano', 4, 'es atigrado muy amigable ', 'pet_ylytSCDS.jpg', 1, '2/12/2019', 1, 'barrio San martin', 12),
+(4, 'leonsio', 'gato', 'chico', 6, 'color blanco y jugueton', 'pet_OBUUuW2Q.jpg', 1, '1/12/2019', 1, 'plaza del cerro', 12),
+(5, 'guancho', 'pequines', 'chico', 4, 'se perdió ...', 'pet_lzHxHJG6.jpg', 1, '3/12/2019', 1, 'las runas', 13),
+(6, 'mocho', 'caniche', 'chico', 3, 'ggjkll\nfghjkk\ngghhjj\n', 'pet_gkR2YSQT.jpg', 1, '5/12/2019', 1, 'San Luis centro', 13),
+(7, 'bruno', 'gatito ', 'chico', 4, 'hjkkk\nrghj\n', 'pet_7iGOrcvw.jpg', 1, '5/12/2019', 1, 'barrio cgt', 13);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `provincia`
+-- Estructura de tabla para la tabla `provincias`
 --
 
-CREATE TABLE `provincia` (
+CREATE TABLE `provincias` (
   `ProvinciaId` int(11) NOT NULL,
   `NombreProvincia` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `provincia`
+-- Volcado de datos para la tabla `provincias`
 --
 
-INSERT INTO `provincia` (`ProvinciaId`, `NombreProvincia`) VALUES
+INSERT INTO `provincias` (`ProvinciaId`, `NombreProvincia`) VALUES
 (1, 'Buenos Aires'),
 (2, 'Catamarca'),
 (3, 'Chacos'),
@@ -124,15 +132,22 @@ INSERT INTO `provincia` (`ProvinciaId`, `NombreProvincia`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `recompensa`
+-- Estructura de tabla para la tabla `recompensas`
 --
 
-CREATE TABLE `recompensa` (
+CREATE TABLE `recompensas` (
   `RecompensaId` int(11) NOT NULL,
   `Monto` decimal(10,0) NOT NULL,
-  `Tiempo` int(11) NOT NULL,
+  `Tiempo` varchar(40) NOT NULL,
   `Estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `recompensas`
+--
+
+INSERT INTO `recompensas` (`RecompensaId`, `Monto`, `Tiempo`, `Estado`) VALUES
+(1, '0', '0', 0);
 
 -- --------------------------------------------------------
 
@@ -144,14 +159,11 @@ CREATE TABLE `usuarios` (
   `UsuarioId` int(11) NOT NULL,
   `Apellido` varchar(30) NOT NULL,
   `Nombre` varchar(30) NOT NULL,
-<<<<<<< HEAD
   `Ciudad` varchar(40) NOT NULL,
-=======
->>>>>>> 347700ac7e0f3e361b2eed4be3e4c6994a987b4a
-  `Direccion` varchar(45) NOT NULL,
   `Telefono` varchar(30) NOT NULL,
   `Email` varchar(30) NOT NULL,
   `Clave` varchar(100) NOT NULL,
+  `Estado` int(11) NOT NULL,
   `ProvinciaId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -159,55 +171,42 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-<<<<<<< HEAD
-INSERT INTO `usuarios` (`UsuarioId`, `Apellido`, `Nombre`, `Ciudad`, `Direccion`, `Telefono`, `Email`, `Clave`, `ProvinciaId`) VALUES
-(12, 'lopez', 'juan jose', '', '9 de julio 810', '2664334455', 'juanlopez@mail.com', '1234', 1),
-(13, 'sosa', 'maria', '', 'alla 123', '2664444444', 'sosamaria@mail.com', '1234', 4),
-(15, 'Lopez', 'Juan', 'San Luis', 'Aca 123', '2664-000000', 'Juan@lopez.com', '1234', 18);
-=======
-INSERT INTO `usuarios` (`UsuarioId`, `Apellido`, `Nombre`, `Direccion`, `Telefono`, `Email`, `Clave`, `ProvinciaId`) VALUES
-(12, 'lopez', 'juan jose', '9 de julio 810', '2664334455', 'juanlopez@mail.com', '1234', 1),
-(13, 'sosa', 'maria', 'alla 123', '2664444444', 'sosamaria@mail.com', '1234', 4);
->>>>>>> 347700ac7e0f3e361b2eed4be3e4c6994a987b4a
+INSERT INTO `usuarios` (`UsuarioId`, `Apellido`, `Nombre`, `Ciudad`, `Telefono`, `Email`, `Clave`, `Estado`, `ProvinciaId`) VALUES
+(12, 'lopez', 'juan jose', 'Villa mercedes ', '2664334455', 'juanlopez@mail.com', '1234', 1, 18),
+(13, 'sosa', 'maria jose', 'San Luis', '266444343', 'sosamaria@mail.com', '1234', 1, 18),
+(15, 'Rodriguez', 'Juan', 'San Luis', '2664-000000', 'rodriguezjuan@mail.com', '1234', 1, 18),
+(16, 'Perez', 'Miguel', 'San Luis', '2664000010', 'perezmiguel@mail.com', '1234', 1, 18),
+(17, 'Avalos', 'Laura', 'Villa Mercedes', '2664000012', 'avaloslaura@mail.com', '1234', 1, 18);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `encontrada`
+-- Indices de la tabla `encontradas`
 --
-ALTER TABLE `encontrada`
+ALTER TABLE `encontradas`
   ADD PRIMARY KEY (`EncontradaId`),
-  ADD KEY `LocalizacionId` (`LocalizacionId`),
-  ADD KEY `MascotaId` (`MascotaId`),
   ADD KEY `UsuarioId` (`UsuarioId`);
-
---
--- Indices de la tabla `localizacion`
---
-ALTER TABLE `localizacion`
-  ADD PRIMARY KEY (`LocalizacionId`);
 
 --
 -- Indices de la tabla `mascotas`
 --
 ALTER TABLE `mascotas`
   ADD PRIMARY KEY (`MascotaId`),
-  ADD KEY `RecompensaId` (`RecompensaId`,`PerdidoEn`),
-  ADD KEY `UsuarioId` (`UsuarioId`),
-  ADD KEY `PerdidoEn` (`PerdidoEn`);
+  ADD KEY `RecompensaId` (`RecompensaId`),
+  ADD KEY `UsuarioId` (`UsuarioId`);
 
 --
--- Indices de la tabla `provincia`
+-- Indices de la tabla `provincias`
 --
-ALTER TABLE `provincia`
+ALTER TABLE `provincias`
   ADD PRIMARY KEY (`ProvinciaId`);
 
 --
--- Indices de la tabla `recompensa`
+-- Indices de la tabla `recompensas`
 --
-ALTER TABLE `recompensa`
+ALTER TABLE `recompensas`
   ADD PRIMARY KEY (`RecompensaId`);
 
 --
@@ -222,75 +221,57 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `encontrada`
+-- AUTO_INCREMENT de la tabla `encontradas`
 --
-ALTER TABLE `encontrada`
-  MODIFY `EncontradaId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `localizacion`
---
-ALTER TABLE `localizacion`
-  MODIFY `LocalizacionId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `encontradas`
+  MODIFY `EncontradaId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `mascotas`
 --
 ALTER TABLE `mascotas`
-  MODIFY `MascotaId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `MascotaId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT de la tabla `provincia`
+-- AUTO_INCREMENT de la tabla `provincias`
 --
-ALTER TABLE `provincia`
+ALTER TABLE `provincias`
   MODIFY `ProvinciaId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT de la tabla `recompensa`
+-- AUTO_INCREMENT de la tabla `recompensas`
 --
-ALTER TABLE `recompensa`
-  MODIFY `RecompensaId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `recompensas`
+  MODIFY `RecompensaId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-<<<<<<< HEAD
-  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-=======
-  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
->>>>>>> 347700ac7e0f3e361b2eed4be3e4c6994a987b4a
+  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `encontrada`
+-- Filtros para la tabla `encontradas`
 --
-ALTER TABLE `encontrada`
-  ADD CONSTRAINT `encontrada_ibfk_1` FOREIGN KEY (`MascotaId`) REFERENCES `mascotas` (`MascotaId`),
-  ADD CONSTRAINT `encontrada_ibfk_2` FOREIGN KEY (`UsuarioId`) REFERENCES `usuarios` (`UsuarioId`);
-
---
--- Filtros para la tabla `localizacion`
---
-ALTER TABLE `localizacion`
-  ADD CONSTRAINT `localizacion_ibfk_1` FOREIGN KEY (`LocalizacionId`) REFERENCES `encontrada` (`LocalizacionId`);
+ALTER TABLE `encontradas`
+  ADD CONSTRAINT `encontradas_ibfk_2` FOREIGN KEY (`UsuarioId`) REFERENCES `usuarios` (`UsuarioId`);
 
 --
 -- Filtros para la tabla `mascotas`
 --
 ALTER TABLE `mascotas`
-  ADD CONSTRAINT `mascotas_ibfk_1` FOREIGN KEY (`RecompensaId`) REFERENCES `recompensa` (`RecompensaId`),
-  ADD CONSTRAINT `mascotas_ibfk_2` FOREIGN KEY (`PerdidoEn`) REFERENCES `localizacion` (`LocalizacionId`),
+  ADD CONSTRAINT `mascotas_ibfk_1` FOREIGN KEY (`RecompensaId`) REFERENCES `recompensas` (`RecompensaId`),
   ADD CONSTRAINT `mascotas_ibfk_3` FOREIGN KEY (`UsuarioId`) REFERENCES `usuarios` (`UsuarioId`);
 
 --
 -- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`ProvinciaId`) REFERENCES `provincia` (`ProvinciaId`);
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`ProvinciaId`) REFERENCES `provincias` (`ProvinciaId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
